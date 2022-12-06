b027be6e29aa0e636b3585ae91f3507a053f13e2
Mango <46423804+Mango-de@users.noreply.github.com>
2022-12-06 19:56:30 +0100

    day 06

---
+++ solutions/06.py
@@ -0,0 +1,31 @@
+with open('inputs/06') as f:
+    t = f.read()
+
+
+def part_1(text: str):
+    quantity = 4
+    for i in range(3, len(text)):
+        t = ''
+        for j in range(quantity):
+            if (char := text[i - j]) in t:
+                break
+            t += char
+        if len(t) == quantity:
+            print(i + 1)
+            break
+
+
+def part_2(text: str):
+    quantity = 14
+    for i in range(3, len(text)):
+        t = ''
+        for j in range(quantity):
+            if (char := text[i - j]) in t:
+                break
+            t += char
+        if len(t) == quantity:
+            print(i + 1)
+            break
+
+part_1(t)
+part_2(t)
