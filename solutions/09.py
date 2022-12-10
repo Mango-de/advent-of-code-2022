943435e7b60733549be62d8bee958242b3383be7
Mango <46423804+Mango-de@users.noreply.github.com>
2022-12-10 23:06:19 +0100

    day 09 cleanup

--- solutions/09.py
+++ solutions/09.py
@@ -1,4 +1,3 @@
-from itertools import pairwise
 from operator import add
 
 with open('inputs/09') as f:
@@ -20,46 +19,8 @@ def diagonally(h, t):
     return h[0] != t[0] and h[1] != t[1]
 
 
-def part_1(l):
-    h = (0, 0)
-    t = (0, 0)
-
-    tail_visited = set()
-    tail_visited.add(t)
-
-    for direction, quantity in l:
-        instruction = tuple(quantity * x for x in directions[direction])
-        h = tuple(map(add, h, instruction))
-        if not touching(h, t):
-            x, y = t
-            if diagonally(h, t):
-                if h[0] > x and h[1] < y:
-                    x += 1; y -= 1
-                elif h[0] > x and h[1] > y:
-                    x += 1; y += 1
-                elif h[0] < x and h[1] < y:
-                    x -= 1; y -= 1
-                elif h[0] < x and h[1] > y:
-                    x -= 1; y += 1
-                t = (x, y)
-                tail_visited.add(t)
-            while not touching((x, y), h):
-                if h[0] > x:
-                    x += 1
-                elif h[1] > y:
-                    y += 1
-                elif h[0] < x:
-                    x -= 1
-                elif h[1] < y:
-                    y -= 1
-                t = (x, y)
-                tail_visited.add(t)
-
-    print(len(tail_visited))
-
-
-def part_2(l):
-    knots = [(0, 0)] * 10
+def solve(l, n):
+    knots = [(0, 0)] * n
 
     tail_visited = set()
     tail_visited.add(knots[-1])
@@ -67,7 +28,7 @@ def part_2(l):
     for direction, quantity in l:
         for _ in range(quantity):
             knots[0] = tuple(map(add, knots[0], directions[direction]))
-            for i in range(9):
+            for i in range(n - 1):
                 h, t = knots[i:i + 2]
                 if not touching(h, t):
                     x, y = t
@@ -81,7 +42,7 @@ def part_2(l):
                         elif h[0] < x and h[1] > y:
                             x -= 1; y += 1
                         knots[i + 1] = (x, y)
-                        if i == 8:
+                        if i == n - 2:
                             tail_visited.add((x, y))
                     while not touching((x, y), h):
                         if h[0] > x:
@@ -93,10 +54,18 @@ def part_2(l):
                         elif h[1] < y:
                             y -= 1
                         knots[i + 1] = (x, y)
-                        if i == 8:
+                        if i == n - 2:
                             tail_visited.add((x, y))
 
-    print(len(tail_visited))
+    return len(tail_visited)
+
+
+def part_1(l):
+    print(solve(l, 2))
+
+
+def part_2(l):
+    print(solve(l, 10))
 
 part_1(l)
 part_2(l)
