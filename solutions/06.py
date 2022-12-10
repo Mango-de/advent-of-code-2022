e53c69e5451921a4d97785ec05f07a3934003633
Mango <46423804+Mango-de@users.noreply.github.com>
2022-12-10 23:11:56 +0100

    day 06 cleanup

--- solutions/06.py
+++ solutions/06.py
@@ -2,8 +2,7 @@ with open('inputs/06') as f:
     t = f.read()
 
 
-def part_1(text: str):
-    quantity = 4
+def solve(text: str, quantity: int):
     for i in range(quantity - 1, len(text)):
         t = ''
         for j in range(quantity):
@@ -11,21 +10,15 @@ def part_1(text: str):
                 break
             t += char
         if len(t) == quantity:
-            print(i + 1)
-            break
+            return i + 1
+
+
+def part_1(text: str):
+    print(solve(text, 4))
 
 
 def part_2(text: str):
-    quantity = 14
-    for i in range(quantity - 1, len(text)):
-        t = ''
-        for j in range(quantity):
-            if (char := text[i - j]) in t:
-                break
-            t += char
-        if len(t) == quantity:
-            print(i + 1)
-            break
+    print(solve(text, 14))
 
 part_1(t)
 part_2(t)
