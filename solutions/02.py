d3a019e18b87878c77294f3fd135866e531568e1
Mango <46423804+Mango-de@users.noreply.github.com>
2022-12-10 23:19:31 +0100

    day 02 cleanup

--- solutions/02.py
+++ solutions/02.py
@@ -2,41 +2,38 @@ with open('inputs/02') as f:
     l = [*map(lambda x: x.split(' '), f.read().splitlines())]
 
 
-def part_1(l):
-    values = {
-        'X': 1,
-        'Y': 2,
-        'Z': 3
-    }
-
-    scores = {
-        'A': {
-            'X': 3,
-            'Y': 6,
-            'Z': 0
-        },
-        'B': {
-            'X': 0,
-            'Y': 3,
-            'Z': 6
-        },
-        'C': {
-            'X': 6,
-            'Y': 0,
-            'Z': 3
-        }
-    }
-
+def solve(l: list[str], scores: dict[str, dict[str, int]]):
     score = 0
 
     for a, b in l:
-        score += values[b]
         score += scores[a][b]
 
-    print(score)
+    return score
 
 
-def part_2(l):
+def part_1(l: list[str]):
+    scores = {
+        'A': {
+            'X': 4,
+            'Y': 8,
+            'Z': 3
+        },
+        'B': {
+            'X': 1,
+            'Y': 5,
+            'Z': 9
+        },
+        'C': {
+            'X': 7,
+            'Y': 2,
+            'Z': 6
+        }
+    }
+
+    print(solve(l, scores))
+
+
+def part_2(l: list[str]):
     scores = {
         'A': {
             'X': 3,
@@ -55,12 +52,7 @@ def part_2(l):
         }
     }
 
-    score = 0
-
-    for a, b in l:
-        score += scores[a][b]
-
-    print(score)
+    print(solve(l, scores))
 
 part_1(l)
 part_2(l)
