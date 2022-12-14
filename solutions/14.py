9ea24e925fd2f7cac9e53c644dbe657c5f34f370
Mango <46423804+Mango-de@users.noreply.github.com>
2022-12-14 17:42:35 +0100

    day 14 fix typehints

--- solutions/14.py
+++ solutions/14.py
@@ -7,7 +7,7 @@ with open('inputs/14') as f:
 directions = [(0, 1), (-1, 1), (1, 1)]
 
 
-def get_rocks(paths: list[tuple[int, int]]) -> set:
+def get_rocks(paths: list[list[tuple[int, int]]]) -> set:
     rocks = set()
 
     for path in paths:
@@ -29,7 +29,7 @@ def get_rocks(paths: list[tuple[int, int]]) -> set:
     return rocks
 
 
-def part_1(paths: list[tuple[int, int]]):
+def part_1(paths: list[list[tuple[int, int]]]):
     blocked = get_rocks(paths)
 
     success = True
@@ -57,7 +57,7 @@ def part_1(paths: list[tuple[int, int]]):
     print(sand)
 
 
-def part_2(paths: list[tuple[int, int]]):
+def part_2(paths: list[list[tuple[int, int]]]):
     blocked = get_rocks(paths)
     ground = 2 + max([r[1] for r in blocked])
 
