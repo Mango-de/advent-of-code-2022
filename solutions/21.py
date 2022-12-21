c3b038e8b87c37470f226c2a29927eb5a65f3be6
Mango <46423804+Mango-de@users.noreply.github.com>
2022-12-21 22:07:35 +0100

    day 21

--- solutions/21.py
+++ solutions/21.py
@@ -1,6 +1,8 @@
 import re
-from functools import lru_cache
-from operator import add, sub, mul, floordiv
+from functools import cache
+from operator import add, floordiv, mul, sub
+
+from sympy import parse_expr, solve
 
 with open('inputs/21') as f:
     data = dict(map(lambda x: re.match(r'([a-z]+): (.+)', x).groups(), f.read().splitlines()))
@@ -13,8 +15,8 @@ operations = {
 }
 
 
-@lru_cache(maxsize=None)
-def calculate(key: str):
+@cache
+def calculate(key: str) -> int:
     if (value := data[key]).isdigit():
         return int(value)
 
@@ -22,13 +24,23 @@ def calculate(key: str):
     return operations[op](calculate(k1), calculate(k2))
 
 
+@cache
+def make_equation(key: str) -> str:
+    if (value := data[key]).isdigit():
+        return 'x' if key == 'humn' else value
+
+    k1, op, k2 = value.split()
+    return f'({make_equation(k1)}) {op} ({make_equation(k2)})'
+
+
 def part_1():
     print(calculate('root'))
 
 
-# def part_2():
-#     pass
+def part_2():
+    left, _, right = data['root'].split()
+    print(solve(sub(*map(parse_expr, map(make_equation, (left, right)))), 'x')[0])
 
 
 part_1()
-# part_2()
+part_2()
